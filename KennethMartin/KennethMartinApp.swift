f5e4763453796610ee54e256902576430e67ecc5
Berk GOZEK <144159613+BerkAlpGozek@users.noreply.github.com>
2023-09-10 16:48:08 +0300

    Initial Commit

---
+++ KennethMartin/KennethMartinApp.swift
@@ -0,0 +1,17 @@
+//
+//  KennethMartinApp.swift
+//  KennethMartin
+//
+//  Created by Berk Gozek on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct KennethMartinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
